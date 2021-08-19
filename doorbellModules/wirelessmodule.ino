b0ff9a41477c5fcf34b35a83f6ace750a316432a
mmg20 <promagicpl@gmail.com>
2021-08-19 10:49:22 +0200

    Add files via upload
    wireless module

---
+++ doorbellModules/wirelessmodule.ino
@@ -0,0 +1,31 @@
+#include <RCSwitch.h>
+#include <avr/sleep.h>
+
+
+#define ledPin 1
+
+RCSwitch mySwitch = RCSwitch();
+
+void usp () {
+  set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+  ADCSRA &= ~(1<<ADEN);       // Turn off ADC to save power
+  sleep_enable();
+  sleep_cpu();
+}
+
+void setup() {
+ pinMode(ledPin, OUTPUT);
+ mySwitch.enableTransmit(0);
+ mySwitch.setRepeatTransmit(15);
+
+}
+
+void loop() {
+   
+   digitalWrite(1, HIGH);
+   mySwitch.send("000000000001010100010001");
+   delay(3000);
+   digitalWrite(1, LOW); 
+   usp();
+   }
+
